61bbe89bd4d9b66bbd981e943a9ba9576a8933a2
SilentCicero <thenickdodson@gmail.com>
2020-06-20 21:21:03 -0400

    v1.0.0 css and design

--- config/build.js
+++ config/build.js
@@ -4,7 +4,7 @@ const fs = require('fs').promises;
 const fse = require('fs-extra');
 const showdown  = require('showdown');
 const { markdownToTxt } = require('markdown-to-txt');
-const converter = new showdown.Converter();
+const converter = new showdown.Converter({ tables: true });
 
 const src = './src/';
 const dist = './dist/';
@@ -13,8 +13,8 @@ const index = './build/index.html';
 
 const treeToPanel = _tree => _tree
   .map(v => v.children.length
-      ? `<div><h2>${title(v.name)}</h2> <div>${treeToPanel(v.children)}</div></div>`
-      : `<a href="${normalize(v.path)}">${title(v.name)}</a>`)
+      ? `<div class="group"><h2 class="group-header">${title(v.name)}</h2> <div class="group-children">${treeToPanel(v.children)}</div></div>`
+      : `<a class="link" href="${normalize(v.path)}">${title(v.name)}</a>`)
   .join('');
 
 const filterVersion = (_tree, version = 'v1.0.0') => _tree.filter(v => v.name === version)[0].children;
@@ -22,7 +22,7 @@ const filterVersion = (_tree, version = 'v1.0.0') => _tree.filter(v => v.name ==
 (async () => {
   let indexFile = false;
   const arr = list(src);
-  const arrFiles = JSON.stringify(arr.map(normalize));
+  const arrFiles = JSON.stringify(arr);
   const header = await fs.readFile(index, 'utf8');
   const panel = treeToPanel(filterVersion(tree(src)));
 
@@ -30,16 +30,22 @@ const filterVersion = (_tree, version = 'v1.0.0') => _tree.filter(v => v.name ==
 
   // copy parcel build over
   for (const file of list(build)) {
-    const content = await fs.readFile(path.join(build, file), 'utf8');
-    await fse.outputFile(path.join(dist, file), content);
+    // const content = await fs.readFile(path.join(build, file), 'utf8');
+    // await fse.outputFile(path.join(dist, file), content);
+
+    await fse.copy(path.join(build, file), path.join(dist, file));
   }
 
   // build markdown
-  for (const file of list(src)) {
+  var i = 0;
+  for (const file of arr) {
     const content = await fs.readFile(path.join(src, file), 'utf8');
     const html = header
       .replace("'%%files%%'", arrFiles)
       .replace('"%%files%%"', arrFiles)
+      .replace('%%file%%', file)
+      .replace('%%nextTitle%%', title(arr[i + 1] || arr[0]))
+      .replace('%%next%%', normalize(arr[i + 1] || arr[0]))
       .replace('%%title%%', title(file))
       .replace('%%content%%', converter.makeHtml(content))
       .replace('%%panel%%', panel);
@@ -50,6 +56,7 @@ const filterVersion = (_tree, version = 'v1.0.0') => _tree.filter(v => v.name ==
       await fse.outputFile(path.join(dist, 'index.html'), html);
       indexFile = true;
     }
+    i++;
   }
 
 })();
--- public/index.js
+++ public/index.js
@@ -1,1 +1,121 @@
-console.log('hello world');
+import "regenerator-runtime";
+import { h, app } from 'hyperapp';
+import styled from 'hyperapp-styled-components';
+
+function is_numeric(str){
+  return /^\d+$/.test(str);
+}
+
+const normalize = file => file
+  .split('/')
+  .map(v => (is_numeric(v[0]) ? v.slice(v.indexOf('.') + 1) : v).split(' ').join('_'))
+  .join('__')
+  .replace('&', 'and')
+  .replace('.md', '.html');
+
+const title = file => normalize(file.split('/').pop())
+  .split('_')
+  .join(' ')
+  .trim()
+  .replace('.md', '')
+  .replace('.html', '');
+
+
+function tree(list = []) {
+  let result = [];
+  let level = {result};
+
+  list.forEach(path => {
+    path.slice(1).split('/').reduce((r, name, i, a) => {
+      if(!r[name]) {
+        r[name] = { result: [] };
+        r.result.push({ name, dir: normalize(path.slice(1).split('/').slice(0, i + 1).join('/')), path, children: r[name].result });
+      }
+      return r[name];
+    }, level)
+  });
+
+  return result;
+}
+
+const treeToPanel2 = (_tree, state = {}) => _tree
+  .map(v => v.children.length
+      ? (<div id={v.dir} class="group">
+          <h2 class="group-header" onclick={state => ({ ...state, open: v.dir })}>{title(v.name)}</h2>
+          <div class={'group-children ' + (
+            (state.pathname.indexOf(v.dir) !== -1
+              || state.open.indexOf(v.dir) !== -1 ? 'group-open' : '')
+          )}>{treeToPanel2(v.children, state)}</div>
+        </div>)
+      : (<a class={'link ' + (
+          state.pathname.indexOf(normalize(v.path)) !== -1 ? 'link-open' : ''
+        )} href={normalize(v.path)}>{title(v.name)}</a>));
+
+const filterVersion = (_tree, version = 'v1.0.0') => _tree.filter(v => v.name === version)[0].children;
+
+function search(pattern) {
+  document.getElementById('content').innerHTML = '';
+
+  for (const file of window.files.map(normalize)) {
+    window.axios.get('/' + file + '.md')
+    .then(function (response) {
+
+      const fuse = new Fuse([{
+        title: response.config.url,
+        content: response.data,
+      }], {
+        keys: [
+          "title",
+          "content",
+        ],
+      });
+
+      const result = fuse.search(pattern);
+
+      if (result.length) {
+        result.map(({ item }) => {
+          const itemHTML = `
+            <div>
+              <h3><a href="${item.title.replace('.md', '')}">
+                ${item.title.replace('.md', '').replace('.html', '').split('___').slice(-1)}
+              </a></h3>
+              <p>${item.content.slice(0, 500)}...</p>
+            </div>
+          `;
+
+          document.getElementById('content').innerHTML += itemHTML;
+        });
+      }
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+  }
+}
+
+const view = state => (
+  <div>
+    <div id="panel-header">
+      <input type="text" id="search" placeholder="Search..." onkeyup={(state, event) => {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+          search(event.target.value);
+        }
+        return state;
+      }} />
+      <a href="#">v1.0.0</a>
+    </div>
+    {treeToPanel2(filterVersion(tree(window.files)), state)}
+  </div>
+);
+
+app({
+  init: {
+    open: '',
+    pathname: window.location.pathname,
+  },
+  view,
+  subscriptions: state => [],
+  node: document.getElementById('panel'),
+});
