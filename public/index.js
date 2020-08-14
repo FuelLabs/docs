b7f46864c1d84a8f91841dfbec11aeb9ed0764c5
SilentCicero <thenickdodson@gmail.com>
2020-08-14 12:26:28 -0400

    version padding, search scroll, anchor fix, vanishing logo fix, hover states, search focus

--- public/index.js
+++ public/index.js
@@ -1,11 +1,22 @@
 import "regenerator-runtime";
 import { h, app } from 'hyperapp';
 import styled from 'hyperapp-styled-components';
-import importRegex from 'esm-import-regex';
 
-window.importRegex = importRegex;
+// set the defualt rinkeby url for transactions to use in the v1.0.0/Getting Started
 window.rinkebyTx = 'https://rinkeby.fuel.sh/tx/';
 
+// fix table squashing on mobile by wrapping with overflow scrolling
+const tables = document.getElementsByTagName('table');
+for (let index = 0; index < tables.length; index += 1){
+  const table = tables[index];
+  const tableWrapper = document.createElement("div");
+  tableWrapper.classList.add("table_wrapper");
+  const parent = table.parentNode;
+  parent.replaceChild(tableWrapper, table);
+  tableWrapper.appendChild(table);
+}
+
+// for the rinkeby script in docs
 function linkify(text) {
     var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
     return text.replace(urlRegex, function(url) {
@@ -13,6 +24,7 @@ function linkify(text) {
     });
 }
 
+// for the rinkeby script
 window.consoleLog = (...args) => {
   document.getElementById('run-console').innerHTML += `
     <div class="run-console-entry">${args.map((arg) => {
@@ -100,83 +112,70 @@ const treeToPanel2 = (_tree, state = {}) => _tree
           <span class="dot"></span>{title(v.name)}
         </a>));
 
-/*
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/styles/xcode.min.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/highlight.min.js"></script>
-
-hljs.initHighlightingOnLoad();
-
-<link rel="stylesheet"
-  href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/styles/tomorrow-night.min.css">
-
-document
-  .getElementById('search')
-  .addEventListener("keyup", event => {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-      search(event.target.value);
-    }
-});
-*/
-
 const filterVersion = (_tree, version = 'v1.0.0') => _tree.filter(v => v.name === version)[0].children;
 
 // TODO fix search
-function search(pattern) {
+async function search(pattern) {
   document.getElementById('content').innerHTML = '<h1>Search Results</h1>';
   document.getElementById('panel-wrapper').classList.toggle("panel-open");
   document.getElementById('content').classList.toggle("panel-closed");
   document.getElementById('hamburger').classList.toggle("is-active");
+  const queries = [];
+  let numberOfResults = 0;
 
   for (const file of window.files.map(normalize)) {
-    window.axios.get('https://docs.fuel.sh' + file + '.md')
-    .then(function (response) {
+    queries.push(window.axios.get('https://docs.fuel.sh' + file + '.md')
+      .then(function (response) {
 
-      const fuse = new Fuse([{
-        title: response.config.url,
-        content: response.data,
-      }], {
-        keys: [
-          "title",
-          "content",
-        ],
-      });
-
-      let result = fuse.search(pattern);
-      const clean = v => v.replace('https://docs.fuel.sh/v1.0.0/', '')
-        .split('__').join(': ').split('_').join(' ');
-
-      if (result.length === 0 && response.data.indexOf(pattern) !== -1) {
-        result.push({ item: {
+        const fuse = new Fuse([{
           title: response.config.url,
           content: response.data,
-        }});
-      }
-
-      if (result.length) {
-        result.map(({ item }) => {
-          const itemHTML = `
-            <div class="search-item">
-              <h3><a href="${item.title.replace('.md', '')}">
-                ${clean(item.title
-                  .replace('.md', '')
-                  .replace('.html', '')
-                  .split('___')
-                  .slice(-1)[0])}
-              </a></h3>
-              <p>${item.content.slice(0, 500)}...</p>
-            </div>
-          `;
-
-          document.getElementById('content').innerHTML += itemHTML;
+        }], {
+          keys: [
+            "title",
+            "content",
+          ],
         });
-      }
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    });
+
+        let result = fuse.search(pattern);
+        const clean = v => v.replace('https://docs.fuel.sh/v1.0.0/', '')
+          .split('__').join(': ').split('_').join(' ');
+
+        if (result.length === 0 && response.data.indexOf(pattern) !== -1) {
+          result.push({ item: {
+            title: response.config.url,
+            content: response.data,
+          }});
+        }
+
+        if (result.length) {
+          numberOfResults += 1;
+          result.map(({ item }) => {
+            const itemHTML = `
+              <div class="search-item">
+                <h3><a href="${item.title.replace('.md', '')}">
+                  ${clean(item.title
+                    .replace('.md', '')
+                    .replace('.html', '')
+                    .split('___')
+                    .slice(-1)[0])}
+                </a></h3>
+                <p>${item.content.slice(0, 500)}...</p>
+              </div>
+            `;
+
+            document.getElementById('content').innerHTML += itemHTML;
+          });
+        }
+
+        return Promise.resolve();
+      }));
+  }
+
+  await Promise.all(queries);
+
+  if (!numberOfResults) {
+    document.getElementById('content').innerHTML += `<div id="invaid_search">Hmm, looks like we couldn't find anything.</div>`;
   }
 }
 
@@ -192,6 +191,7 @@ const view = state => (
       <span>Fuel</span>
     </a>
     <div id="panel-header">
+      <div id="panel-header-fill"></div>
       <div id="search-wrapper">
         <input type="text" id="search" placeholder="Search..." onkeypress={(state, event) => {
           if (event.keyCode === 13) {
